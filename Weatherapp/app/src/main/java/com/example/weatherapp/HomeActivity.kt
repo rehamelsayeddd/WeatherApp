f959d812b1af959324a9c2daa1a427113d1e5ed7
rehamelsayedd294 <rehamelsayeddd@yahoo.com>
2024-09-18 00:54:51 +0300

    editing on home screen ui

--- Weatherapp/app/src/main/java/com/example/weatherapp/HomeActivity.kt
+++ Weatherapp/app/src/main/java/com/example/weatherapp/HomeActivity.kt
@@ -7,10 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-       
+
     }
 }
