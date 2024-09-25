7a5f8411a8bb75487003023658bea8a899497cf4
rehamelsayedd294 <rehamelsayeddd@yahoo.com>
2024-09-25 19:10:16 +0300

    adding favourite state sealed class

---
+++ Weatherapp/app/src/main/java/com/example/weatherapp/FavouriteState.kt
@@ -0,0 +1,11 @@
+package com.example.weatherapp
+
+import com.example.weatherapp.ForecastModel.CurrentWeatherResponse
+
+// Sealed class for representing the FAVOURITE_FRAGMENT
+
+sealed class FavouriteState {
+    object Loading : FavouriteState()
+    data class Success(val favourites: List<CurrentWeatherResponse>) : FavouriteState() // Updated to List
+    data class Error(val message: String) : FavouriteState()
+}
