f8841bfc5f56089c475d18466e067fc86bedffa3
rehamelsayedd294 <rehamelsayeddd@yahoo.com>
2024-09-28 20:24:26 +0300

    making alert takes from irepo interface

--- Weatherapp/app/src/main/java/com/example/weatherapp/Alert/ViewModel/AlertViewModel.kt
+++ Weatherapp/app/src/main/java/com/example/weatherapp/Alert/ViewModel/AlertViewModel.kt
@@ -5,11 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.weatherapp.OneCall.Model.AlertData
+import com.example.weatherapp.Repository.IWeatherRepository
 import com.example.weatherapp.Repository.WeatherRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class AlertViewModel (private val repo: WeatherRepository): ViewModel() {
+class AlertViewModel (private val repo: IWeatherRepository): ViewModel() {
     private var _alertData: MutableLiveData<List<AlertData>> = MutableLiveData<List<AlertData>>()
     val alerts: LiveData<List<AlertData>> = _alertData
     init {
